9cb959e07f05f73c5c985d41f6385f566dd129f0
yuetsh <517252939@qq.com>
2022-09-04 15:06:01 +0800

    docs.

--- src/components/mobile/Helper.jsx
+++ src/components/mobile/Helper.jsx
@@ -8,13 +8,27 @@ import styles from "./Helper.module.css"
 function PythonHelper() {
   return (
     <React.Fragment>
-      <Button onClick={onHelper("\t")}>Tab</Button>
-      <Button onClick={onHelper("()")}>{"( )"}</Button>
-      <Button onClick={onHelper('""')}>{'" "'}</Button>
-      <Button onClick={onHelper("''")}>{"' '"}</Button>
-      <Button onClick={onHelper(" = ")}>{"="}</Button>
-      <Button onClick={onHelper("input()")}>输入</Button>
-      <Button onClick={onHelper("print()")}>输出</Button>
+      <Button size="large" onClick={onHelper("\t")}>
+        Tab
+      </Button>
+      <Button size="large" onClick={onHelper("()")}>
+        {"( )"}
+      </Button>
+      <Button size="large" onClick={onHelper('""')}>
+        {'" "'}
+      </Button>
+      <Button size="large" onClick={onHelper("''")}>
+        {"' '"}
+      </Button>
+      <Button size="large" onClick={onHelper(" = ")}>
+        {"="}
+      </Button>
+      <Button size="large" onClick={onHelper("input()")}>
+        输入
+      </Button>
+      <Button size="large" onClick={onHelper("print()")}>
+        输出
+      </Button>
     </React.Fragment>
   )
 }
@@ -22,24 +36,63 @@ function PythonHelper() {
 function CHelper() {
   return (
     <React.Fragment>
-      <Button onClick={onHelper("\t")}>Tab</Button>
-      <Button onClick={onHelper(",")}>{","}</Button>
-      <Button onClick={onHelper(";")}>{";"}</Button>
-      <Button onClick={onHelper("<>")}>{"< >"}</Button>
-      <Button onClick={onHelper("()")}>{"( )"}</Button>
-      <Button onClick={onHelper("[]")}>{"[ ]"}</Button>
-      <Button onClick={onHelper("{\n")}>{"{ }"}</Button>
-      <Button onClick={onHelper('""')}>{'" "'}</Button>
-      <Button onClick={onHelper("''")}>{"' '"}</Button>
-      <Button onClick={onHelper("%d")}>%d</Button>
-      <Button onClick={onHelper("%f")}>%f</Button>
-      <Button onClick={onHelper("%s")}>%s</Button>
-      <Button onClick={onHelper("&")}>{"&"}</Button>
-      <Button onClick={onHelper(" = ")}>{"="}</Button>
-      <Button onClick={onHelper("int ")}>{"int"}</Button>
-      <Button onClick={onHelper("float ")}>{"float"}</Button>
-      <Button onClick={onHelper('scanf("");')}>输入</Button>
-      <Button onClick={onHelper('printf("");')}>输出</Button>
+      <Button size="large" onClick={onHelper("\t")}>
+        Tab
+      </Button>
+      <Button size="large" onClick={onHelper(",")}>
+        {","}
+      </Button>
+      <Button size="large" onClick={onHelper(";")}>
+        {";"}
+      </Button>
+      <Button size="large" onClick={onHelper("\\n")}>
+        {"\\n"}
+      </Button>
+      <Button size="large" onClick={onHelper("<>")}>
+        {"< >"}
+      </Button>
+      <Button size="large" onClick={onHelper("()")}>
+        {"( )"}
+      </Button>
+      <Button size="large" onClick={onHelper("[]")}>
+        {"[ ]"}
+      </Button>
+      <Button size="large" onClick={onHelper("{\n")}>
+        {"{ }"}
+      </Button>
+      <Button size="large" onClick={onHelper('""')}>
+        {'" "'}
+      </Button>
+      <Button size="large" onClick={onHelper("''")}>
+        {"' '"}
+      </Button>
+      <Button size="large" onClick={onHelper("%d")}>
+        %d
+      </Button>
+      <Button size="large" onClick={onHelper("%f")}>
+        %f
+      </Button>
+      <Button size="large" onClick={onHelper("%s")}>
+        %s
+      </Button>
+      <Button size="large" onClick={onHelper("&")}>
+        {"&"}
+      </Button>
+      <Button size="large" onClick={onHelper(" = ")}>
+        {"="}
+      </Button>
+      <Button size="large" onClick={onHelper("int ")}>
+        {"int"}
+      </Button>
+      <Button size="large" onClick={onHelper("float ")}>
+        {"float"}
+      </Button>
+      <Button size="large" onClick={onHelper('scanf("");')}>
+        输入
+      </Button>
+      <Button size="large" onClick={onHelper('printf("");')}>
+        输出
+      </Button>
     </React.Fragment>
   )
 }
