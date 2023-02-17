b39886c8791e9f927c9a176213278bbd22b3723b
xuyue <517252939@qq.com>
2023-02-17 11:03:56 +0800

    test for chrome 66.

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import * as monaco from "monaco-editor"
 import { loader } from "@monaco-editor/react"
 import App from "./components/App"
 import { getChromeVersion } from "./utils"
 
-const isLowVersion = getChromeVersion() < 70
+const isLowVersion = getChromeVersion() > 70
 
-if (!isLowVersion) {
+if (isLowVersion) {
+  loader.config({ monaco })
+} else {
   loader.config({
     paths: { vs: "https://cdn.staticfile.org/monaco-editor/0.34.1/min/vs" },
     "vs/nls": { availableLanguages: { "*": "zh-cn" } },
