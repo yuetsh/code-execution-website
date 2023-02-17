7b880911c062c5d3649ae72ac2f0f3a745938d32
xuyue <517252939@qq.com>
2023-02-17 10:56:37 +0800

    test for chrome 66.

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,13 @@ import App from "./components/App"
 import { getChromeVersion } from "./utils"
 
 const isLowVersion = getChromeVersion() < 70
-const monacoUrl = isLowVersion
-  ? "https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.30.1/min/vs"
-  : "https://cdn.staticfile.org/monaco-editor/0.34.1/min/vs"
 
-loader.config({
-  paths: { vs: monacoUrl },
-  "vs/nls": { availableLanguages: { "*": "zh-cn" } },
-})
+if (!isLowVersion) {
+  loader.config({
+    paths: { vs: "https://cdn.staticfile.org/monaco-editor/0.34.1/min/vs" },
+    "vs/nls": { availableLanguages: { "*": "zh-cn" } },
+  })
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-
 root.render(<App />)
