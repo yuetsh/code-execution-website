7079af2d964441f1567d40b97b2e6a6ac2c7e500
yuetsh <517252939@qq.com>
2023-04-03 19:13:51 +0800

    delete local monaco-editor

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import * as monaco from "monaco-editor"
 import { loader } from "@monaco-editor/react"
 import App from "./components/App"
 import { isLowVersion } from "./utils"
 
-if (isLowVersion) {
-  loader.config({ monaco })
-} else {
-  loader.config({
-    paths: { vs: "https://cdn.staticfile.org/monaco-editor/0.34.1/min/vs" },
-    "vs/nls": { availableLanguages: { "*": "zh-cn" } },
-  })
-}
+const version = isLowVersion ? "0.30.1" : "0.36.1"
+const protocol = isLowVersion ? "http" : "https"
+
+loader.config({
+  paths: {
+    vs: `${protocol}://cdn.staticfile.org/monaco-editor/${version}/min/vs`,
+  },
+  "vs/nls": { availableLanguages: { "*": "zh-cn" } },
+})
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
 root.render(<App />)
